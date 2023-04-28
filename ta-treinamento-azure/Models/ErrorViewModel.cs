ebd7d0ab43aa4652273871092241adead05ded1f
Elayne Natália <elayne.argollo@solutis.com.br>
2023-04-28 16:11:09 -0300

    Nova aplicação web mvc

---
+++ ta-treinamento-azure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ta_treinamento_azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
